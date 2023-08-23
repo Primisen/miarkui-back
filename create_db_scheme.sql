4de7772c454d184613fff33891da28dd6366a6bb
Nadia Shokel <primisen17@gmail.com>
2023-08-23 20:41:47 +0200

    Added DB description

---
+++ create_db_scheme.sql
@@ -0,0 +1,121 @@
+CREATE DATABASE miarkui;
+
+USE miarkui;
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(10) NOT NULL
+);
+
+INSERT INTO role (name) VALUE ("ADMIN");
+INSERT INTO role (name) VALUE ("USER");
+
+CREATE TABLE user (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role_id INT NOT NULL,
+    username VARCHAR(20) NOT NULL,
+    email VARCHAR(20) NOT NULL,
+    password VARCHAR(512) NOT NULL,
+
+    FOREIGN KEY (role_id)
+        REFERENCES role(id),
+
+    UNIQUE(username)
+);
+
+CREATE TABLE category (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20) NOT NULL
+);
+
+INSERT INTO category (name) VALUE ("MOVIE");
+INSERT INTO category (name) VALUE ("BOOK");
+INSERT INTO category (name) VALUE ("GAME");
+
+CREATE TABLE subject (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    category_id INT NOT NULL,
+
+    FOREIGN KEY (category_id)
+        REFERENCES category(id)
+);
+
+CREATE TABLE rating (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    rating SMALLINT NOT NULL,
+    subject_id INT NOT NULL,
+    user_id INT NOT NULL,
+
+    FOREIGN KEY (user_id)
+        REFERENCES user(id),
+
+    FOREIGN KEY (subject_id)
+        REFERENCES subject(id)
+);
+
+CREATE TABLE review (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(25) NOT NULL,
+    subject_id INT NOT NULL,
+    user_id INT NOT NULL,
+    text TEXT NOT  NULL,
+
+    FOREIGN KEY (subject_id)
+        REFERENCES subject(id),
+
+    FOREIGN KEY (user_id)
+        REFERENCES user(id)
+);
+
+CREATE TABLE image (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    url VARCHAR(1000) NOT NULL,
+    review_id INT NOT NULL,
+
+    FOREIGN KEY (review_id)
+        REFERENCES review(id)
+);
+
+CREATE TABLE comment (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    text TEXT,
+    user_id INT NOT NULL,
+    review_id INT NOT NULL,
+
+    FOREIGN KEY (user_id)
+        REFERENCES user(id),
+
+    FOREIGN KEY (review_id)
+        REFERENCES review(id)
+);
+
+CREATE TABLE likes (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    review_id INT NOT NULL,
+
+    FOREIGN KEY (user_id)
+        REFERENCES user(id),
+
+    FOREIGN KEY (review_id)
+        REFERENCES review(id)
+);
+
+CREATE TABLE tag(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE tag_review (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tag_id INT NOT NULL,
+    review_id INT NOT NULL,
+
+    FOREIGN KEY (tag_id)
+        REFERENCES tag(id),
+
+    FOREIGN KEY (review_id)
+        REFERENCES review(id)
+);
+
