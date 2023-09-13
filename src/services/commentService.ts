233f74a081d4f4e0f9bd0e9d9265786a7c5093ca
Nadia Shokel <primisen17@gmail.com>
2023-09-13 22:58:43 +0200

    Set up adding data to OpenSearch index

--- src/services/commentService.ts
+++ src/services/commentService.ts
@@ -1,17 +1,24 @@
 import { Request } from 'express';
-import { Comment } from '../models/comment.js'
+import { Comment } from '../models/comment.js';
+import OpenSearch from '../opensearch/openSearch.js'
+import { openSearchIndexName } from '../constants/openSearchIndexName.js'
 
 class CommentService {
-  async create(request: Request) {
-    const comment = new Comment({
-      userId: request.body.userId,
-      reviewId: request.body.reviewId,
-      text: request.body.text
-    });
+    async create(request: Request) {
+        const comment = new Comment({
+            userId: request.body.user.id,
+            reviewId: request.body.reviewId,
+            text: request.body.text,
+        });
 
-    console.log("comment: " + comment)
-    return comment.save();
-  }
+        const savedComment= await comment.save();
+        await OpenSearch.getClient().index({
+            index: openSearchIndexName,
+            body: savedComment,
+        });
+
+        return savedComment
+    }
 }
 
 export default new CommentService();
--- src/services/ratingService.ts
+++ src/services/ratingService.ts
@@ -1,24 +1,23 @@
 import { Request } from 'express';
-import { Rating } from '../models/rating.js'
+import { Rating } from '../models/rating.js';
 
 class RatingService {
-  async create(request: Request) {
-    const rating = new Rating({
-      userId: request.body.userId,
-      subjectId: request.body.subjectId,
-      score: request.body.score
-    });
-    await rating.save();
-    return rating;
-  }
+    async create(request: Request) {
+        const rating = new Rating({
+            userId: request.body.userId,
+            subjectId: request.body.subjectId,
+            score: request.body.score,
+        });
+        return rating;
+    }
 
-  async get(request: Request) {
-    return Rating.findAll({
-      where: {
-        subjectId: request.body.subjectId
-      }
-    })
-  }
+    async get(request: Request) {
+        return Rating.findAll({
+            where: {
+                subjectId: request.body.subjectId,
+            },
+        });
+    }
 }
 
 export default new RatingService();
